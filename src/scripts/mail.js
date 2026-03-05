d06f410131a25211c5eab40721592a00a90cd36b
Gian-Luca <gianluca.loy.dev@gmail.com>
2026-03-05 22:10:16 +0100

    Update_05_03_2026_Backend
    Überarbeitung Spielmechanik.

--- src/scripts/mail.js
+++ src/scripts/mail.js
@@ -1,59 +1,53 @@
-import { loadSimulatorData, openAspectMenu } from './main.js';
+import { openAspectMenu } from './main.js';
 
-document.addEventListener('DOMContentLoaded', async () => {
-    const simulatorData = await loadSimulatorData();
-    showMail(simulatorData);
-});
-
-function showMail(simulatorData) {
+export function showMail(suggestedMail) {
     const mailContainer = document.getElementById('mail-container');
+    mailContainer.innerHTML = '';
 
-    simulatorData.mails.forEach(mail => {
-        console.log(mail);
-        const mailEl = document.createElement('div');
-        mailEl.className = 'mail-element';
+    const mailEl = document.createElement('div');
+    mailEl.className = 'mail-element';
         
-        const mailSubject = document.createElement('p');
-        mailSubject.className = 'mail-subject';
-        mailSubject.textContent = `Betreff: ${mail.subject}`;
+    const mailSubject = document.createElement('p');
+    mailSubject.className = 'mail-subject';
+    mailSubject.textContent = `Betreff: ${suggestedMail.subject}`;
 
-        const mailSender = document.createElement('p');
-        mailSender.className = 'mail-sender';
-        mailSender.textContent = `von: ${mail.sender}`;
+    const mailSender = document.createElement('p');
+    mailSender.className = 'mail-sender';
+    mailSender.textContent = `von: ${suggestedMail.sender}`;
 
-        const mailInformation = document.createElement('p');
-        mailInformation.className = 'mail-information';
-        mailInformation.textContent = mail.information;
+    const mailInformation = document.createElement('p');
+    mailInformation.className = 'mail-information';
+    mailInformation.textContent = suggestedMail.information;
 
-        const eventSuggestion = document.createElement('span');
-        eventSuggestion.className = 'event-suggestion';
-        eventSuggestion.textContent = mail.eventSuggestion.title;
-        eventSuggestion.addEventListener('click', () => {
-            openAspectMenu(mail.eventSuggestion.type);
-            setTimeout(() => {
-                const targetEvent = document.getElementById(mail.eventSuggestion.id);
+    const eventSuggestion = document.createElement('span');
+    eventSuggestion.className = 'event-suggestion';
+    eventSuggestion.textContent = suggestedMail.eventSuggestion.title;
+    eventSuggestion.addEventListener('click', () => {
+        openAspectMenu(suggestedMail.eventSuggestion.type);
+        setTimeout(() => {
+            const targetEvent = document.getElementById(suggestedMail.eventSuggestion.id);
+            if (targetEvent) {
                 const targetEventRejectBtn = targetEvent.querySelector('.reject-btn');
-                if (targetEvent) {
-                    targetEvent.scrollIntoView({behaviour: 'smooth', block: 'start'});
-                    targetEvent.classList.add('event-card-highlighted');
-                    // Reveals the reject-btn for the highlighted event
-                    targetEventRejectBtn.classList.remove('hide-reject-btn');
-                } 
-            }, 50);
-        });
-
-        const mailHeader = document.createElement('div');
-        mailHeader.className = 'mail-header';
-        mailHeader.appendChild(mailSubject);
-        mailHeader.appendChild(mailSender);
-
-        const mailContent = document.createElement('div');
-        mailContent.className = 'mail-content';
-        mailContent.appendChild(mailInformation);
-        mailContent.appendChild(eventSuggestion);
-
-        mailEl.appendChild(mailHeader);
-        mailEl.appendChild(mailContent);
-        mailContainer.appendChild(mailEl);
+                
+                targetEvent.scrollIntoView({behavior: 'smooth', block: 'start'});
+                targetEvent.classList.add('event-card-highlighted');
+                // Reveals the reject-btn for the highlighted event
+                targetEventRejectBtn.classList.remove('hide-reject-btn');
+            } 
+        }, 50);
     });
+
+    const mailHeader = document.createElement('div');
+    mailHeader.className = 'mail-header';
+    mailHeader.appendChild(mailSubject);
+    mailHeader.appendChild(mailSender);
+
+    const mailContent = document.createElement('div');
+    mailContent.className = 'mail-content';
+    mailContent.appendChild(mailInformation);
+    mailContent.appendChild(eventSuggestion);
+
+    mailEl.appendChild(mailHeader);
+    mailEl.appendChild(mailContent);
+    mailContainer.appendChild(mailEl);
 }
