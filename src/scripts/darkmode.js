22c06880f193044e454d03fc5f363ad7ae7fc39d
Skygaming229 <luke.seitter@outlook.de>
2026-03-12 20:44:37 +0100

    Änderungen 12.03.26
    Anfänge Darkmode
    Insta account velinkt
    Datenschutz fertig
    Logo umbennannt

---
+++ src/scripts/darkmode.js
@@ -0,0 +1,22 @@
+let darkmode = localStorage.getItem("darkmode");
+const darkmodeToggle = document.getElementById("darkmode-toggle");
+
+const enableDarkmode = () => {
+    document.body.classList.add("darkmode");
+    localStorage.setItem("darkmode", "enabled");
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove("darkmode");
+    localStorage.setItem("darkmode", "disabled");
+}
+
+if (darkmode === "enabled") {
+    enableDarkmode();
+} else {
+    disableDarkmode();
+}
+
+darkmodeToggle.addEventListener("click", () => {
+    darkmode = localStorage.getItem("darkmode");
+});
